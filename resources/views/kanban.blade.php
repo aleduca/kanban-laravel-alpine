44ded1246e1e33845e2cce8111475d3c6b33a6d5
Alexandre Eduardo Cardoso <alecar2007@gmail.com>
2025-10-11 10:21:46 -0300

    create data to server

--- resources/views/kanban.blade.php
+++ resources/views/kanban.blade.php
@@ -14,9 +14,25 @@
     </header>
 
     <main class="grid gap-6 grid-cols-1 md:grid-cols-3" x-data="{
+      cols:[],
+      createDataToUpdateKanban: function(tasks,columnName,columnId){
+          tasks.forEach((task,key) => {
+            let newPosition = key+1;
+            task.setAttribute('x-sort:item', newPosition);
+            this.cols[columnName] ??= {};
+            this.cols[columnName][columnId] ??= [];
+            this.cols[columnName][columnId].push({
+              [task.dataset.id]:newPosition
+            })
+          })
+      },
       sorted: function(event){
+        {{-- console.log(event); --}}
         const fromTasks = event.from.querySelectorAll('article');
+        const toTasks = event.to.querySelectorAll('article');
         const noTaskElement = event.to.querySelector('#no-task');
+        const columnIdFrom = event.from.dataset.col;
+        const columnIdTo = event.to.dataset.col;
 
         if(fromTasks.length <= 0){
           event.from.innerHTML = `<article class='block text-center' id='no-task'>Nenhuma tarefa</article>`
@@ -25,7 +41,31 @@
         if(noTaskElement){
           noTaskElement.remove();
         }
-        console.log(event.from);
+
+        this.createDataToUpdateKanban(fromTasks,'from',columnIdFrom);
+
+        if(columnIdFrom !== columnIdTo){
+          this.createDataToUpdateKanban(toTasks,'to',columnIdTo);
+        }
+
+        console.log(this.cols);
+
+        this.cols = [];
+
+         /*
+        [
+          'from': 2 => [
+            {
+              taskId: newPosition
+            }
+          ],
+          'to': 3 => [
+            {
+              taskId: newPosition
+            }
+          ]
+        ]
+        */
       }
     }">
       <!-- Coluna Peparando -->
@@ -33,9 +73,9 @@
         <header class="px-4 py-3 border-b border-gray-200">
           <h2 class="text-lg font-semibold bg-red-400 rounded text-white text-center">🔴Preparando</h2>
         </header>
-        <div class="flex-1 overflow-y-auto p-4 space-y-4" x-sort x-sort:group="kanban" @end="sorted">
+        <div class="flex-1 overflow-y-auto p-4 space-y-4" data-col="1" x-sort x-sort:group="kanban" @end="sorted">
           @forelse ($tasks[1] ?? [] as $task)
-            <article class="bg-gray-50 p-4 rounded-lg shadow-sm border border-gray-200 cursor-pointer" x-sort:item="{{ $task->position_id }}">
+            <article class="bg-gray-50 p-4 rounded-lg shadow-sm border border-gray-200 cursor-pointer" x-sort:item="{{ $task->position_id }}" data-id="{{ $task->id }}">
             <h3 class="font-medium text-gray-800">{{ $task->title }}</h3>
             <p class="text-sm text-gray-500">
               {{ $task->description }}
@@ -52,9 +92,9 @@
         <header class="px-4 py-3 border-b border-gray-200">
           <h2 class="text-lg font-semibold bg-orange-400 rounded text-white text-center">🟠Desenvolvendo</h2>
         </header>
-          <div class="flex-1 overflow-y-auto p-4 space-y-4" x-sort x-sort:group="kanban" @end="sorted">
+          <div class="flex-1 overflow-y-auto p-4 space-y-4" data-col="2" x-sort x-sort:group="kanban" @end="sorted">
            @forelse ($tasks[2] ?? []  as $task)
-            <article class="bg-gray-50 p-4 rounded-lg shadow-sm border border-gray-200 cursor-pointer" x-sort:item="{{ $task->position_id }}">
+            <article class="bg-gray-50 p-4 rounded-lg shadow-sm border border-gray-200 cursor-pointer" x-sort:item="{{ $task->position_id }}" data-id="{{ $task->id }}">
             <h3 class="font-medium text-gray-800">{{ $task->title }}</h3>
             <p class="text-sm text-gray-500">
               {{ $task->description }}
@@ -71,9 +111,9 @@
         <header class="px-4 py-3 border-b border-gray-200">
           <h2 class="text-lg font-semibold bg-green-400 rounded text-white text-center">🟢Pronto</h2>
         </header>
-        <div class="flex-1 overflow-y-auto p-4 space-y-4" x-sort x-sort:group="kanban" @end="sorted">
+        <div class="flex-1 overflow-y-auto p-4 space-y-4" data-col="3" x-sort x-sort:group="kanban" @end="sorted">
           @forelse ($tasks[3] ?? [] as $task)
-            <article class="bg-gray-50 p-4 rounded-lg shadow-sm border border-gray-200 cursor-pointer" x-sort:item="{{ $task->position_id }}">
+            <article class="bg-gray-50 p-4 rounded-lg shadow-sm border border-gray-200 cursor-pointer" x-sort:item="{{ $task->position_id }}" data-id="{{ $task->id }}">
             <h3 class="font-medium text-gray-800">{{ $task->title }}</h3>
             <p class="text-sm text-gray-500">
               {{ $task->description }}
